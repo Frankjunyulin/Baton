685080609438ca1ebebb15d14df257099961992a
frankjunyulin <frankjunyulin@gmail.com>
2022-10-08 19:59:54 +0800

    Add create task modal

---
+++ baton/pages/components/TaskCreate.tsx
@@ -0,0 +1,104 @@
+import {Fragment, useState} from "react";
+import {Dialog, Transition} from "@headlessui/react";
+import {CheckIcon} from "@heroicons/react/24/outline";
+
+type Props = {
+  isCreateModalOpen: boolean;
+  setCreateModalOpen: (val: boolean) => void;
+  taskList: Any;
+  setTaskList: (val: list<Any>) => void;
+};
+
+export default function TaskCreate({
+  isCreateModalOpen,
+  setCreateModalOpen,
+  taskList,
+  setTaskList,
+}: Props) {
+  const [taskName, setTaskName] = useState("");
+  console.log(taskName);
+
+  return (
+    <Transition.Root show={isCreateModalOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setCreateModalOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  {/*form----------- */}
+                  <form className="space-y-6" action="#" method="POST">
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Name
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="name"
+                          type="text"
+                          name="name"
+                          onChange={(event) => setTaskName(event.target.value)}
+                          required
+                          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                        onClick={() => {
+                          setCreateModalOpen(false);
+                          setTaskList([
+                            ...taskList,
+                            {
+                              id: taskList[taskList.length - 1].id + 1,
+                              title: taskName,
+                              initials: "GA",
+                              team: "Engineering",
+                              assignees: [],
+                              totalAssignees: 12,
+                              lastUpdated: "March 17, 2020",
+                              pinned: true,
+                              bgColorClass: "bg-pink-600",
+                            },
+                          ]);
+                        }}
+                      >
+                        Create
+                      </button>
+                    </div>
+                  </form>
+                  {/*form----------- */}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
--- baton/pages/index.tsx
+++ baton/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import BaseLayout from "./components/BaseLayout";
-import Sidebar from "./components/Sidebar";
+import MainContainer from "./components/MainContainer";
 
 const Home: NextPage = () => {
   return (
@@ -13,21 +13,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Manage your team's tasks" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Sidebar />
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <MainContainer />
     </div>
   );
 };
